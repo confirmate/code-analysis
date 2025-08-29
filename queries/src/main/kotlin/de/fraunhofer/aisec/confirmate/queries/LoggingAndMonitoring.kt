4a588cee2e27b53d247b76fabe4aed4b670e8ced
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2025-08-29 11:28:07 +0200

    Formatting

--- queries/src/main/kotlin/de/fraunhofer/aisec/confirmate/queries/LoggingAndMonitoring.kt
+++ queries/src/main/kotlin/de/fraunhofer/aisec/confirmate/queries/LoggingAndMonitoring.kt
@@ -1,3 +1,6 @@
+/*
+ * This file is part of the Confirmate project.
+ */
 package de.fraunhofer.aisec.confirmate.queries
 
 import de.fraunhofer.aisec.cpg.TranslationResult
@@ -5,7 +8,6 @@ import de.fraunhofer.aisec.cpg.graph.Backward
 import de.fraunhofer.aisec.cpg.graph.GraphToFollow
 import de.fraunhofer.aisec.cpg.graph.Interprocedural
 import de.fraunhofer.aisec.cpg.graph.Node
-import de.fraunhofer.aisec.cpg.graph.concepts.logging.LogLevel
 import de.fraunhofer.aisec.cpg.graph.concepts.logging.LogWrite
 import de.fraunhofer.aisec.cpg.query.Must
 import de.fraunhofer.aisec.cpg.query.QueryTree
@@ -13,24 +15,24 @@ import de.fraunhofer.aisec.cpg.query.allExtended
 import de.fraunhofer.aisec.cpg.query.executionPath
 
 context(translationResult: TranslationResult)
-fun relevantActivityHasLogging(relevantActivitiesSpecifier: ((Node) -> Boolean)): QueryTree<Boolean>
-{
+fun relevantActivityHasLogging(
+    relevantActivitiesSpecifier: ((Node) -> Boolean)
+): QueryTree<Boolean> {
     return translationResult.allExtended<Node>(relevantActivitiesSpecifier) { relevantActivity ->
         executionPath(
             startNode = relevantActivity,
             direction = Backward(GraphToFollow.EOG),
             type = Must,
             scope = Interprocedural(),
-            predicate = { node ->
-                node is LogWrite && node.isLevelEnabled
-            }
+            predicate = { node -> node is LogWrite && node.isLevelEnabled },
         )
     }
 }
 
 context(translationResult: TranslationResult)
-fun relevantActivityHasLoggingWithMeaningfulMessage(relevantActivitiesSpecifier: ((Node) -> Boolean)): QueryTree<Boolean>
-{
+fun relevantActivityHasLoggingWithMeaningfulMessage(
+    relevantActivitiesSpecifier: ((Node) -> Boolean)
+): QueryTree<Boolean> {
     return translationResult.allExtended<Node>(relevantActivitiesSpecifier) { relevantActivity ->
         executionPath(
             startNode = relevantActivity,
@@ -38,25 +40,28 @@ fun relevantActivityHasLoggingWithMeaningfulMessage(relevantActivitiesSpecifier:
             type = Must,
             scope = Interprocedural(),
             predicate = { node ->
-                node is LogWrite && node.isLevelEnabled && messageMatchesActivity(
-                    node.logArguments,
-                    relevantActivity
-                )
-            }
+                node is LogWrite &&
+                    node.isLevelEnabled &&
+                    messageMatchesActivity(node.logArguments, relevantActivity)
+            },
         )
     }
 }
 
 val LogWrite.isLevelEnabled: Boolean
     get() {
-        // TODO: Implement proper log level threshold checking. We need to know what is configured for the respective logger.
-        //   There are several ways to achieve this, e.g., by looking at the configuration files or by tracking
-        //   logger initialization in the code. We should probably add a field to the Concept and add a check like:
+        // TODO: Implement proper log level threshold checking. We need to know what is configured
+        // for the respective logger.
+        //   There are several ways to achieve this, e.g., by looking at the configuration files or
+        // by tracking
+        //   logger initialization in the code. We should probably add a field to the Concept and
+        // add a check like:
         //   return this.logLevel >= this.concept.logLevelThreshold
         return true
     }
 
 fun messageMatchesActivity(arguments: List<Node>, relevantActivity: Node): Boolean {
-    // TODO: Maybe use an AI agent to decide if the message constructed by the arguments is meaningful for the activity.
+    // TODO: Maybe use an AI agent to decide if the message constructed by the arguments is
+    // meaningful for the activity.
     return true
 }
