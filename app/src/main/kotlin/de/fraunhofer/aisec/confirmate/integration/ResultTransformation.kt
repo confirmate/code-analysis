fcea1153380a443f3bf3a6836c87779178a12e84
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2025-09-17 17:05:41 +0200

    Formatting, use sets instead of lists

--- app/src/main/kotlin/de/fraunhofer/aisec/confirmate/integration/ResultTransformation.kt
+++ app/src/main/kotlin/de/fraunhofer/aisec/confirmate/integration/ResultTransformation.kt
@@ -14,48 +14,52 @@ import kotlin.uuid.Uuid
 const val codyzeToolId = "Codyze"
 
 @OptIn(ExperimentalUuidApi::class)
-fun AnalysisResult.toClouditorResults(): Pair<List<AssessmentResult>, List<Evidence>> {
-    val currentTimestamp = OffsetDateTime.now() // TODO: Should this be the timestamp when we started the analysis?
+fun AnalysisResult.toClouditorResults(): Pair<Set<AssessmentResult>, Set<Evidence>> {
+    val currentTimestamp =
+        OffsetDateTime.now() // TODO: Should this be the timestamp when we started the analysis?
 
     val toeId = this.translationResult.id.toString()
 
-    val evidences = mutableListOf<Evidence>()
+    val evidences = mutableSetOf<Evidence>()
 
-    val assessmentResults = this.requirementsResults.map { (requirementId, result) ->
-        evidences.add(
-            Evidence(
-                id = result.id.toString(),
-                timestamp = currentTimestamp,
-                cloudServiceId = null,
-                toolId = codyzeToolId,
-                raw = result.printNicely(),
-                resource = null,
-            )
-        )
+    val assessmentResults =
+        this.requirementsResults
+            .map { (requirementId, result) ->
+                evidences.add(
+                    Evidence(
+                        id = result.id.toString(),
+                        timestamp = currentTimestamp,
+                        cloudServiceId = null,
+                        toolId = codyzeToolId,
+                        raw = result.printNicely(),
+                        resource = null,
+                    )
+                )
 
-        AssessmentResult(
-            id = Uuid.random().toString(),
-            createdAt = currentTimestamp,
-            metricId = requirementId,
-            metricConfiguration =
-                MetricConfiguration(
-                    operator = "==",
-                    targetValue = true,
+                AssessmentResult(
+                    id = Uuid.random().toString(),
+                    createdAt = currentTimestamp,
                     metricId = requirementId,
+                    metricConfiguration =
+                        MetricConfiguration(
+                            operator = "==",
+                            targetValue = true,
+                            metricId = requirementId,
+                            targetOfEvaluationId = toeId,
+                            isDefault = true,
+                        ),
+                    compliant = result.value,
+                    evidenceId = result.id.toString(),
+                    resourceId = "TODO",
+                    resourceTypes = listOf(),
+                    complianceComment = result.printNicely(),
                     targetOfEvaluationId = toeId,
-                    isDefault = true,
-                ),
-            compliant = result.value,
-            evidenceId = result.id.toString(),
-            resourceId = "TODO",
-            resourceTypes = listOf(),
-            complianceComment = result.printNicely(),
-            targetOfEvaluationId = toeId,
-            toolId = codyzeToolId,
-            historyUpdatedAt = currentTimestamp,
-            history = listOf(),
-            complianceDetails = listOf(),
-        )
-    }
+                    toolId = codyzeToolId,
+                    historyUpdatedAt = currentTimestamp,
+                    history = listOf(),
+                    complianceDetails = listOf(),
+                )
+            }
+            .toSet()
     return assessmentResults to evidences
 }
