f5d2c6fbb000f83fb1b7ef9eed44faff299cf24f
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2025-09-19 13:50:36 +0200

    Try to implement in-transit encryption

--- queries/src/main/kotlin/de/fraunhofer/aisec/confirmate/queries/Confidentiality.kt
+++ queries/src/main/kotlin/de/fraunhofer/aisec/confirmate/queries/Confidentiality.kt
@@ -4,17 +4,22 @@
 package de.fraunhofer.aisec.confirmate.queries
 
 import de.fraunhofer.aisec.cpg.TranslationResult
+import de.fraunhofer.aisec.cpg.assumptions.AssumptionType
+import de.fraunhofer.aisec.cpg.assumptions.assume
 import de.fraunhofer.aisec.cpg.graph.Interprocedural
 import de.fraunhofer.aisec.cpg.graph.Node
 import de.fraunhofer.aisec.cpg.graph.concepts.crypto.encryption.Cipher
 import de.fraunhofer.aisec.cpg.graph.concepts.crypto.encryption.Encrypt
 import de.fraunhofer.aisec.cpg.graph.concepts.file.WriteFile
+import de.fraunhofer.aisec.cpg.graph.concepts.http.HttpEndpointOperation
+import de.fraunhofer.aisec.cpg.graph.concepts.http.HttpRequest
 import de.fraunhofer.aisec.cpg.query.GenericQueryOperators
 import de.fraunhofer.aisec.cpg.query.QueryTree
 import de.fraunhofer.aisec.cpg.query.allExtended
 import de.fraunhofer.aisec.cpg.query.alwaysComesFrom
 import de.fraunhofer.aisec.cpg.query.and
 import de.fraunhofer.aisec.cpg.query.mergeWithAll
+import de.fraunhofer.aisec.cpg.query.or
 
 /**
  * This query checks whether each path leading to a persistent data sink encrypts the data before
@@ -32,30 +37,37 @@ fun dataEncryptedBeforePersisting(
         val writtenData =
             writtenData(it)
                 ?: return@allExtended QueryTree(
-                    value = false,
-                    stringRepresentation = "Missing data written to a persistent location",
-                    node = it,
-                    operator = GenericQueryOperators.EVALUATE,
-                )
-
-        val relevantEncryptOperations = mutableListOf<Encrypt>()
-        val writtenDataIsEncrypted =
-            writtenData.alwaysComesFrom(
-                scope = Interprocedural(),
-                predicate = { enc ->
-                    if (enc is Encrypt) {
-                        relevantEncryptOperations.add(enc)
-                        true
-                    } else {
-                        false
-                    }
-                },
-            )
-
-        // The data must be encrypted and the encryption must be state of the art
-        writtenDataIsEncrypted and
-            relevantEncryptOperations.map { it.concept.conformsToStateOfTheArt() }.mergeWithAll()
+                        value = true,
+                        stringRepresentation = "Missing data written to a persistent location",
+                        node = it,
+                        operator = GenericQueryOperators.EVALUATE,
+                    )
+                    .assume(
+                        AssumptionType.CompletenessAssumption,
+                        "We did not find the data written to the persistent sink which might lead to false positives. This behavior is strange as the sink should typically have a way to persist data.\n\nTo verify this assumption, please check if the sink has indeed no way to persist data. To fix this issue, either specify the way how to identify data (by providing an improved `writtenData` argument or consider removing the sink from the `isPersistentSink` argument.",
+                    )
+        writtenData.alwaysCorrectlyEncrypted()
     }
+}
+
+fun Node.alwaysCorrectlyEncrypted(): QueryTree<Boolean> {
+    val relevantEncryptOperations = mutableListOf<Encrypt>()
+    val writtenDataIsEncrypted =
+        this.alwaysComesFrom(
+            scope = Interprocedural(),
+            predicate = { enc ->
+                if (enc is Encrypt) {
+                    relevantEncryptOperations.add(enc)
+                    true
+                } else {
+                    false
+                }
+            },
+        )
+
+    // The data must be encrypted and the encryption must be state of the art
+    return writtenDataIsEncrypted and
+        relevantEncryptOperations.map { it.concept.conformsToStateOfTheArt() }.mergeWithAll()
 }
 
 fun Cipher.conformsToStateOfTheArt(): QueryTree<Boolean> {
@@ -71,3 +83,66 @@ fun Cipher.conformsToStateOfTheArt(): QueryTree<Boolean> {
         operator = GenericQueryOperators.EVALUATE,
     )
 }
+
+context(translationResult: TranslationResult)
+fun dataInTransitEncrypted(): QueryTree<Boolean> {
+
+    TODO()
+}
+
+context(translationResult: TranslationResult)
+fun secureHttpRequests(): QueryTree<Boolean> =
+    translationResult.allExtended<HttpRequest> {
+        // Check if the request is sent over TLS. That's already a good starting point.
+        // TODO: We should actually check the exact TLS configuration (i.e., version, ciphers, ...),
+        // but that's not modeled in the CPG yet.
+        val isSecureChannel = it.concept.isTLS
+        val secureChannel =
+            QueryTree(
+                value = isSecureChannel == true,
+                stringRepresentation =
+                    if (isSecureChannel == true) {
+                        "Data is sent over a secure channel (TLS)"
+                    } else {
+                        "Data is sent over an insecure channel (no TLS)"
+                    },
+                node = it,
+                operator = GenericQueryOperators.EVALUATE,
+            )
+
+        // Check if all arguments of the request are encrypted before the request is sent
+        val dataEncryptedBeforeRequest =
+            it.arguments.map { arg -> arg.alwaysCorrectlyEncrypted() }.mergeWithAll()
+
+        // We need a secure channel or the data must be encrypted before sending
+        secureChannel or dataEncryptedBeforeRequest
+    }
+
+context(translationResult: TranslationResult)
+fun secureHttpResponses(): QueryTree<Boolean> =
+    // We start with the HttpEndpointOperations as they represent the server-side handling of
+    // requests
+    translationResult.allExtended<HttpEndpointOperation> {
+        // Get the HttpClient and check if it's configured to use (only) TLS.
+        val isSecureChannel = it.concept.isTLS
+        val secureChannel =
+            QueryTree(
+                value = isSecureChannel == true,
+                stringRepresentation =
+                    if (isSecureChannel == true) {
+                        "Data is sent over a secure channel (TLS)"
+                    } else {
+                        "Data is sent over an insecure channel (no TLS)"
+                    },
+                node = it,
+                operator = GenericQueryOperators.EVALUATE,
+            )
+
+        // Check if data sent via the endpoint (i.e., the responses) are encrypted before the
+        // response is sent
+        val dataEncryptedBeforeResponse =
+            it.prevDFG.map { arg -> arg.alwaysCorrectlyEncrypted() }.mergeWithAll()
+
+        // We need a secure channel or the data must be encrypted before sending
+        secureChannel or dataEncryptedBeforeResponse
+    }
