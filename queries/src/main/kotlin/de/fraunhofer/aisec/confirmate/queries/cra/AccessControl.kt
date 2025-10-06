27512d7ce7683153476e109689b4265fcc89a621
Konrad Weiss <konrad.weiss@aisec.fraunhofer.de>
2025-10-06 14:15:41 +0200

    Putting it together

--- queries/src/main/kotlin/de/fraunhofer/aisec/confirmate/queries/cra/AccessControl.kt
+++ queries/src/main/kotlin/de/fraunhofer/aisec/confirmate/queries/cra/AccessControl.kt
@@ -21,13 +21,14 @@ import de.fraunhofer.aisec.cpg.query.Must
 import de.fraunhofer.aisec.cpg.query.QueryTree
 import de.fraunhofer.aisec.cpg.query.allExtended
 import de.fraunhofer.aisec.cpg.query.executionPath
+import de.fraunhofer.aisec.cpg.query.mergeWithAll
 
 val endpoints = listOf(HttpEndpoint::class)
 
 context(translationResult: TranslationResult)
 fun authenticationAtEndpoint(
     isAuthentication: (Node) -> Boolean,
-) = getEndpoints().all { endpoint ->
+) = getEndpoints().map { endpoint ->
     executionPath(
         startNode = endpoint,
         direction = Forward(GraphToFollow.EOG),
@@ -36,13 +37,13 @@ fun authenticationAtEndpoint(
     ) {
         isAuthentication(it)
     }
-}
+}.mergeWithAll()
 
 
 context(translationResult: TranslationResult)
 fun authorizationAtEndpoint(
     isAuthorization: (Node) -> Boolean,
-) = getEndpoints().all { endpoint ->
+) = getEndpoints().map { endpoint ->
     executionPath(
         startNode = endpoint,
         direction = Forward(GraphToFollow.EOG),
@@ -51,29 +52,76 @@ fun authorizationAtEndpoint(
     ) {
         isAuthorization(it)
     }
+}.mergeWithAll()
+
+context(translationResult: TranslationResult)
+fun authenticationBeforeCriticalFunctionality(
+    isAuthentication: (Node) -> Boolean,
+isCritical: (Node) -> Boolean) = getCriticalFunctionality(isCritical).map { endpoint ->
+    executionPath(
+        startNode = endpoint,
+        direction = Forward(GraphToFollow.EOG),
+        type = Must,
+        scope = Interprocedural(),
+    ) {
+        isAuthentication(it)
+    }
+}.mergeWithAll()
+
+
+context(translationResult: TranslationResult)
+fun authorizationBeforeCriticalFunctionality(
+    isAuthorization: (Node) -> Boolean,
+    isCritical: (Node) -> Boolean
+): QueryTree<Boolean>  = getCriticalFunctionality(isCritical).map { endpoint ->
+        executionPath(
+            startNode = endpoint,
+            direction = Forward(GraphToFollow.EOG),
+            type = Must,
+            scope = Interprocedural(),
+        ) {
+            isAuthorization(it)
+        }
+    }.mergeWithAll()
+
+/**
+ * Currently this check evaluates to true if there is any logging reachable from the negative branch of a node,
+ * considered to be evaluation. This could be refined, although we expect to not have merging baths of a successful
+ * and unsuccessful authentication/authorization.
+ */
+context(translationResult: TranslationResult)
+fun loggingOnSecurityErrors(
+    isAuthentication: (Node) -> Boolean,
+    isAuthorization: (Node) -> Boolean,
+    ): QueryTree<Boolean> {
+    val errorNodes = getSecurityErrors(getAuthentication(isAuthentication)).union(getSecurityErrors(getAuthorization(isAuthorization)))
+
+
+    return errorNodes.map {errorNode -> executionPath(
+        startNode = errorNode,
+        direction = Forward(GraphToFollow.EOG),
+        type = Must,
+        scope = Interprocedural(),
+    ) {
+        true// Logging
+    }
+}.mergeWithAll()
 }
 
-fun authenticationBeforeCriticalFunctionality(): QueryTree<Boolean> {
-    // Todo getCriticalFunctionality
-}
-
-fun authorizationBeforeCriticalFunctionality(): QueryTree<Boolean> {
-    // Todo getCriticalFunctionality
-}
-
-fun loggingOnSecurityErrors(): QueryTree<Boolean> {
-    // Todo make a Union of  authenticationErrors and authorizationErrors
-}
+context(translationResult: TranslationResult)
+fun getAuthentication(    isAuthentication: (Node) -> Boolean,) = translationResult.allChildrenWithOverlays<Node>(isAuthentication)
 
 
-fun authenticationErrors(): QueryTree<Boolean> {
+context(translationResult: TranslationResult)
+fun getAuthorization(    isAuthorization: (Node) -> Boolean,) = translationResult.allChildrenWithOverlays<Node>(isAuthorization)
 
-}
+/**
+ * This function return the first node after a branch that leads to a path that will be executed after an evaluation to `false`.
+ * This can be used to build a further paths for inspection.
+ */
+context(translationResult: TranslationResult)
+fun getSecurityErrors( securityChecks: List<Node>) = securityChecks.flatMap { it.nextEOGEdges.filter { !(it.branch?: true) }.map { it.end } }
 
-
-fun authorizationErrors(): QueryTree<Boolean> {
-
-}
 
 context(translationResult: TranslationResult)
 fun getEndpoints(): List<Node> {
