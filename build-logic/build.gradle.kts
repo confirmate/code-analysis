a4d02581ff5b560886ad47f37c1d11a75e4b8e40
Alexander Kuechler <alexander.kuechler@aisec.fraunhofer.de>
2025-10-31 15:10:39 +0100

    kover plugin

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 dependencies {
     implementation(libs.kotlin.gradle)
     implementation(libs.kotlin.serialization)
+    implementation(libs.dokka.gradle)
     implementation(libs.kover.gradle)
     implementation(libs.spotless.gradle)
     implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))  // this is only there to be able to import 'LibrariesForLibs' in the convention plugins to access the version catalog in build-logic
